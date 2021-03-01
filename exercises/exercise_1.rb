fac0f9a56bd96ff9180989ead9da0bc0646ed59e
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-01 23:33:47 +0000

    complete exercise 1

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -2,5 +2,13 @@ require_relative '../setup'
 
 puts "Exercise 1"
 puts "----------"
-
+# t.column :name, :string
+# t.column :annual_revenue, :integer
+# t.column :mens_apparel, :boolean
+# t.column :womens_apparel, :boolean
+# t.timestamps null: false
 # Your code goes below here ...
+store1 = Store.create(name: "Burnaby", annual_revenue: 300000, mens_apparel: true, womens_apparel: true)
+store2 = Store.create(name: "Richmond", annual_revenue: 1260000, mens_apparel: false, womens_apparel:true)
+store3 = Store.create(name: "Gastown", annual_revenue: 190000, mens_apparel: true, womens_apparel: false)
+puts Store.count
