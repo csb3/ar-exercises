12692486ec398e8a09fabdc303a0352f42e39c9f
Cass Bredeson <c.s.bredeson@gmail.com>
2021-03-02 01:30:02 +0000

    complete exercise 7

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,11 +9,11 @@ puts "Exercise 6"
 puts "----------"
 
 @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
-@store2.employees.create(first_name: "Bob", last_name: "Loblaw", hourly_rate: 25)
-@store1.employees.create(first_name: "Joel", last_name: "Barish", hourly_rate: 33)
+@store2.employees.create(first_name: "Bob", last_name: "Loblaw", hourly_rate: 45)
+@store1.employees.create(first_name: "Joel", last_name: "Barish", hourly_rate: 50)
 @store2.employees.create(first_name: "Obi-Wan", last_name: "Kenobi", hourly_rate: 61)
-@store1.employees.create(first_name: "Deanna", last_name: "Troi", hourly_rate: 34)
+@store1.employees.create(first_name: "Deanna", last_name: "Troi", hourly_rate: 42)
 @store2.employees.create(first_name: "Tom", last_name: "Hanks", hourly_rate: 58)
-@store1.employees.create(first_name: "Guybrush", last_name: "Threepwood", hourly_rate: 28)
-@store2.employees.create(first_name: "Juliana", last_name: "Crain", hourly_rate: 35)
+@store1.employees.create(first_name: "Guybrush", last_name: "Threepwood", hourly_rate: 99)
+@store2.employees.create(first_name: "Juliana", last_name: "Crain", hourly_rate: 67)
 
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,11 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validate :must_sell_men_or_womens_clothing
+  def must_sell_men_or_womens_clothing
+    unless mens_apparel == true || womens_apparel == true
+      errors.add :base, :invalid, message: "Stores must sell either men's or women's clothing"
+    end
+  end
 end
